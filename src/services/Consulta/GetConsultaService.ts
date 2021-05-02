d3f653c1a3580b32bff5538e49abb0acd34dffb2
ViniciusP78 <viniciusp78@outlook.com.br>
2021-05-02 18:15:04 -0300

    adicionado tratativa de erros

--- src/services/Consulta/GetConsultaService.ts
+++ src/services/Consulta/GetConsultaService.ts
@@ -1,5 +1,7 @@
 import { getRepository } from 'typeorm';
 
+import AppError from '../../errors/AppError';
+
 import Consulta from '../../models/Consulta';
 
 class GetConsultaService{
@@ -7,6 +9,9 @@ class GetConsultaService{
     const consultasRepository = getRepository(Consulta);
 
     const consulta = await consultasRepository.findOne(id, {relations: ["prontuario", "medico"]});
+
+    if(!consulta)
+      throw new AppError('Consulta inexistente', 404);
 
     delete consulta.medico.password;
 
