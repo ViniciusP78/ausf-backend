ccec746f90fecfd85ac41044ec5b5dece2ad599e
ViniciusP78 <viniciusp78@outlook.com.br>
2021-05-02 19:32:16 -0300

    Adicionado busca em consultas

--- src/services/Consulta/ListConsultaService.ts
+++ src/services/Consulta/ListConsultaService.ts
@@ -1,16 +1,31 @@
-import { getRepository} from 'typeorm';
+import { Brackets, getRepository} from 'typeorm';
 
 import Consulta from '../../models/Consulta';
 
 class ListConsultaService {
-  public async execute(): Promise<Consulta[]> {
+  public async execute(search?: string, data?: string): Promise<Consulta[]> {
     const consultasRepository = getRepository(Consulta);
 
-    const consultas = await consultasRepository.find({relations: ["prontuario", "medico"]});
+    // const consultas = await consultasRepository.find({relations: ["prontuario", "medico"]});
+
+    let consultas = await consultasRepository.createQueryBuilder("consulta")
+      .innerJoinAndSelect("consulta.prontuario", "prontuario")
+      .innerJoinAndSelect("consulta.medico", "medico")
+      .innerJoinAndSelect("prontuario.paciente", "paciente")
+      .where(data ? "data_agendada::date = :data" : "1=1",{data})
+      .andWhere(new Brackets(qb => {
+        qb.where("paciente.nome iLIKE :search",{search: search ? `%${search}%` : '%%'})
+        .orWhere("paciente.cartao_sus iLIKE :search",{search: search ? `%${search}%` : '%%'})
+      }))
+      .getMany();
+
 
     consultas.forEach((consulta) => {
       delete consulta.medico.password; //Portante
     })
+
+    
+
 
     return consultas;
   }
