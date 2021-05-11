32a6e341595047cdfd5faaa8612d60b8b88e66f5
Gabriel Feoli <feoli2104@gmail.com>
2021-05-10 23:08:17 -0300

    Route Index atualizado

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,10 @@ import { Router } from 'express';
 import usersRouter from './users.routes';
 import sessionsRouter from './sessions.routes';
 import pacientesRouter from './pacientes.routes';
-import prontuariosRouter from './prontuarios.routes'
-import consultasRouter from './consultas.routes'
-import registrosRouter from './registros.routes'
+import prontuariosRouter from './prontuarios.routes';
+import consultasRouter from './consultas.routes';
+import registrosRouter from './registros.routes';
+import triagensRouter from './triagens.routes';
 
 const routes = Router();
 
@@ -15,5 +16,6 @@ routes.use('/pacientes', pacientesRouter);
 routes.use('/prontuarios', prontuariosRouter);
 routes.use('/consultas', consultasRouter);
 routes.use('/registros', registrosRouter);
+routes.use('/triagens', triagensRouter);
 
 export default routes;
